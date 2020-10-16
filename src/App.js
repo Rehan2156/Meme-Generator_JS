a66f94565570a83cd5b17c83edecbaefbf72b67e
imrehan <rehanshaikh2156@gmail.com>
2020-10-16 21:35:36 +0530

    input buttons with bootstrap ready

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './Header';
 import Meme from './Meme'
+import 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header/>
       <Meme/>
     </div>
