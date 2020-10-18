2b81fe814bc47dc0d7c487c3f123355704f400a2
imrehan <rehanshaikh2156@gmail.com>
2020-10-18 12:42:57 +0530

    Updates

---
+++ precache-manifest.90741d056ad756d9df7cb88b39abf1fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631e17bab7727ad6872481c81f47b090",
+    "url": "/Meme-Generator-React-App/index.html"
+  },
+  {
+    "revision": "081e40bce377fc9043cd",
+    "url": "/Meme-Generator-React-App/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "c184ef9d910d0c52fa77",
+    "url": "/Meme-Generator-React-App/static/css/main.ec82e3ae.chunk.css"
+  },
+  {
+    "revision": "081e40bce377fc9043cd",
+    "url": "/Meme-Generator-React-App/static/js/2.a888c16e.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/Meme-Generator-React-App/static/js/2.a888c16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c184ef9d910d0c52fa77",
+    "url": "/Meme-Generator-React-App/static/js/main.af83031a.chunk.js"
+  },
+  {
+    "revision": "7eb42a5ea2631c7d1d53",
+    "url": "/Meme-Generator-React-App/static/js/runtime-main.bc34e683.js"
+  }
+]);
